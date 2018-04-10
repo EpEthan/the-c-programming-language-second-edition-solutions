6460db360898a25f51b57d17d282a1063a91127b
EpEthan <31385999+EpEthan@users.noreply.github.com>
2018-04-10 20:05:57 +0300

    Create Fold Lines (by EpEthan).c

---
+++ Chapter1/Exercise 1-22/Fold Lines (by EpEthan).c
@@ -0,0 +1,75 @@
+/*
+* Author: EpEthan
+*
+* EpEthan's solution for Exercise 1-22, Chapter1.
+*/
+
+
+#include <stdio.h>
+
+#define MAXLINE 1000	/* maximum length of input line */
+#define PERLINE 75	/* maximum number of character per line */
+
+int getline(char s[]);
+void fold(char from[], char to[]);
+
+
+main()
+{
+	char line[MAXLINE];
+	char output_lines[MAXLINE];
+
+	while (getline(line) > 0) {
+		fold(line, output_lines);
+
+		printf("\n\n---------------------------------FOLD RESULT-----------------------------------\n");
+		printf("%s", output_lines);
+		printf("\n-------------------------------------------------------------------------------\n\n");
+
+	}
+
+	return 0;
+}
+
+
+
+
+/* getline: copies line into s[], returns length */
+int getline(char s[])
+{
+	int i;
+	int c;
+
+	for (i = 0; i < MAXLINE - 1
+		&& (c = getchar()) != 'EOF' && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+
+	return i;
+}
+
+/* fold: turn long input lines into two or more shorter lines after the last non-blanks */
+void fold(char from[], char to[])
+{
+	int i;
+	int chars_in_line;	/* number of characters in the line */
+	int last_blank;	/* index of last blank/tab found */
+
+
+	for (i = 0, chars_in_line = 0; from[i] != '\0'; ++i, ++chars_in_line) {
+		to[i] = from[i];
+
+		if (to[i] == ' ')
+			last_blank = i;
+		if (chars_in_line == PERLINE - 1) {
+			chars_in_line = 0;
+			to[last_blank] = '\n';
+		}
+	}
+
+	to[--i] = '\0';
+}
