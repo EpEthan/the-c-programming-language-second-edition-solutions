62157b46bb23bde0dbb4fe1d368773536ac26832
EpEthan <31385999+EpEthan@users.noreply.github.com>
2018-04-10 17:26:04 +0300

    Create Lines Longer than 80 (by EpEthan).c

---
+++ Chapter1/Exercise 1-17/Lines Longer than 80 (by EpEthan).c
@@ -0,0 +1,49 @@
+/*
+* Author: EpEthan
+* Created using Jeremy Yu's (ccpalettes) solution
+*
+* EpEthan's solution for Exercise 1-17, Chapter1.
+*/
+
+#include <stdio.h>
+
+#define MAXLINE 1000	/* maximum input line size */
+#define MIN_LEN 80	/* minimum length for a line to be printed */
+
+int getline(char line[], int maxline);
+
+/* print all input lines longer than 80 */
+main()
+{
+	int len;	/* current line length */
+	char line[MAXLINE];	/* current input line */
+
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > MIN_LEN) {
+			printf("\n\n--------------------THIS LINE WAS LONGER THAN 80 CHARACTERS!--------------------\n");
+			printf("%s\n", line);
+			printf("--------------------------------------------------------------------------------\n\n");
+		}
+
+	return 0;
+}
+
+
+/* getline: read a line into s, return length */
+int getline(char s[], int limit)
+{
+	int c, i, l;
+
+	for (i = 0, l = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
+		if (i < limit - 1)
+			s[l++] = c;
+	}
+	if (c == '\n') {
+		if (l < limit - 1)
+			s[l++] = c;
+		++i;
+	}
+	s[l] = '\0';
+
+	return i;
+}
